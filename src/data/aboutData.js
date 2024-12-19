e06bd12c4442b8df88ca25dd304cfcf504ead38f
GedeonMatabaro <mugishomatgedeon@gmail.com>
2024-12-19 01:47:23 -0500

    updating the name

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
     title: "About Me",
-    description1: "My name is Ange Iracyadukunda. I'm a fullstack web designer and developer based in Kigali, Rwanda.",
+    description1: "My name is Angelique Iracyadukunda. I'm a fullstack web designer and developer based in Kigali, Rwanda.",
     description2: "As an aspiring leader and computing enthusiast, I am passionate about utilizing technology to drive positive change in the world. I am highly motivated and committed to making a meaningful contribution to society, and I believe that effective leadership is essential to achieving this goal. With a strong commitment to continuous learning,  I am eager to explore new opportunities to enhance my leadership skills and contribute to the betterment of the world.",
 }
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,8 +1,8 @@
 
 export const headerData = {
-    name: 'Ange Iracyadukunda',
+    name: 'Angelique Iracyadukunda',
     title: "",
-    desciption: "I am Ange Iracyadukunda, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about utilizing technology to drive positive change in the world.",
+    desciption: "I am Angelique Iracyadukunda, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about utilizing technology to drive positive change in the world.",
     image: 'https://i.ibb.co/5chws1f/144936972.jpg' ,
     imagebw: 'https://i.ibb.co/4KFWbPc/144936972-Copy.jpg',
     resumePdf: 'https://drive.google.com/file/d/1E8JXi_7Zv1MlHPEt66O3-60otkVURIw9/view?usp=sharing'
